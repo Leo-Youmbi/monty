68bb0d4a7905d4bfe76edf9fcd93fa346f393b71
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-22 21:20:13 +0100

    Implementing add instruction

--- monty.h
+++ monty.h
@@ -14,6 +14,7 @@
 		    {"pint", pint},   \
 		    {"pop", pop},     \
 		    {"swap", swap},   \
+		    {"add", _add},    \
 		    {"nop", nop},   \
 		{                     \
 			NULL, NULL      \
@@ -76,6 +77,8 @@ void pint(stack_t **stack, unsigned int line_cnt);
 void swap(stack_t **stack, unsigned int line_cnt);
 void pop(stack_t **stack, unsigned int line_cnt);
 void nop(stack_t **stack, unsigned int line_cnt);
+
+void _add(stack_t **stack, unsigned int line_cnt);
 
 void opcode(stack_t **stack, char *str, unsigned int line_cnt);
 
